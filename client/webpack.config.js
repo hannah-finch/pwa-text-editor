879eeea20d8a83304f86b743b89dd98b449d055c
hannahschwen <hannahschwen@gmail.com>
2024-08-25 15:33:57 -0400

    wrote webpack pwa manifest to create manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,24 +31,24 @@ module.exports = () => {
       }),
 
       // Creates a manifest.json file.
-      // new WebpackPwaManifest({
-      //   fingerprints: false,
-      //   inject: true,
-      //   name: 'Contact Cards',
-      //   short_name: 'Contact',
-      //   description: 'Never forget your contacts!',
-      //   background_color: '#225ca3',
-      //   theme_color: '#225ca3',
-      //   start_url: './',
-      //   publicPath: './',
-      //   icons: [
-      //     {
-      //       src: path.resolve('src/images/logo.png'),
-      //       sizes: [96, 128, 192, 256, 384, 512],
-      //       destination: path.join('assets', 'icons'),
-      //     },
-      //   ],
-      // }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
