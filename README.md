5e93a8096bf694b4a3314df98b7b64e417a08279
hannahschwen <hannahschwen@gmail.com>
2024-09-04 12:30:14 -0400

    start README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# pwa-text-editor
-Text editor that runs in the browser and offline
+# PWA Text Editor
+
+## Description
+
+Just another text editor that runs in the browser or can be downloaded to work offline.
+
+View the deployed application here: https://pwa-text-editor-w9hd.onrender.com/
+
+
+## Installation
+
+## Usage
+
+## Credits
+
+## License
+
+[MIT License](./LICENSE)
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = () => {
         inject: true,
         name: 'Just Another Text Editor',
         short_name: 'J.A.T.E.',
-        description: 'Takes notes with JavaScript syntax highlighting!',
+        description: 'Take notes with JavaScript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
